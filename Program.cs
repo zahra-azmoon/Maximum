776b765a5799fafbad5bb002b9cbe479a8a7f6e5
zahra-azmoon <zahra.azmoon25@gmail.com>
2025-09-30 19:10:57 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter n : ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int max = 0;
+            for (int i = 0; i < n ; i++)
+            {
+                Console.WriteLine("enter your numbers: ");
+                int numbers = Convert.ToInt32(Console.ReadLine());
+                if (numbers > max)
+                    max = numbers;
+            }
+            Console.Write("maximum is: {0} ",max);
+            
+        }
+    }
+}
